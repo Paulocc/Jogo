8d7820d54f3a61de4bbd20c6d82c5858d92e7e6e
izabela <izabela.fusieger@outlook.com>
2019-10-29 20:24:04 -0300

    primo

--- soma.py
+++ soma.py
@@ -1,3 +1,10 @@
 a=int(input())
 b=int(input())
-print(a+b)
+soma=a+b
+print(soma)
+for i in range (2,soma)
+	if soma%i==0:
+		print(soma,"não é primo")
+		break
+if i==soma-1:
+	print(soma,"é primo") 
