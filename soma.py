09cbaa81f40c586f9a322978a65810c69ef8846b
izabelafusieger <56324835+izabelafusieger@users.noreply.github.com>
2019-11-02 10:59:25 -0300

    soma, confere se é primo

--- soma.py
+++ soma.py
@@ -2,9 +2,12 @@ a=int(input())
 b=int(input())
 soma=a+b
 print(soma)
-for i in range (2,soma):
-	if soma%i==0:
-		print(soma,"não é primo")
-		break
-if i==soma-1:
-	print(soma,"é primo") 
+if soma>1:
+	for i in range (1,soma):
+		if i!=1 and soma%i==0:
+			print(soma,"não é primo")
+			break
+	if i==soma-1 or soma==2:
+		print(soma,"é primo")
+else:
+	print("Entrada inválida")
