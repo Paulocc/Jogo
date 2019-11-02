8acb084a2df3ecbc1b91070b3f50f9e8e615113e
Lucas Bitello <54330590+LucasBitello@users.noreply.github.com>
2019-11-02 10:45:37 -0300

    Update multi.py
    Impressão do quadrado formado por * apartir do resultado

--- multi.py
+++ multi.py
@@ -1,3 +1,14 @@
-n1=int(input())
-n2=int(input())
-print(n1*n2)
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+resul = n1*n2
+for x in range(resul):
+    for i in range(resul):
+        if (resul-x) == resul or (resul-x) == 1:
+            print("*", end="")
+        else:        
+            if (resul-i) == resul or (resul-i) == 1:
+                print("*", end="")
+            else:
+                print(" ", end="") 
+    print()
