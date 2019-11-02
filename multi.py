46a9534f787ae5049532a362b83339cecea8ed4b
Lucas Bitello <54330590+LucasBitello@users.noreply.github.com>
2019-11-02 11:03:06 -0300

    Update multi.py
    Será impresso um print com o resultado

--- multi.py
+++ multi.py
@@ -1,7 +1,7 @@
-
 n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
 resul = n1*n2
+print("O resultado é: ", resul)
 for x in range(resul):
     for i in range(resul):
         if (resul-x) == resul or (resul-x) == 1:
