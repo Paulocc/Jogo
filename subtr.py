d9b6b9e157147c42b2cf109632b2e67c53ccc2ad
luizzanoni <56282554+luizzanoni@users.noreply.github.com>
2019-11-12 19:52:56 -0300

    Update subtr.py
    Atividade de subtração

--- subtr.py
+++ subtr.py
@@ -1,3 +1,23 @@
-n1=int(input())
-n2=int(input())
-print(n1-n2)
+n1=int(input("Digite um numero: "))
+n2=int(input("Digite um numéro: "))
+resul = n1-n2
+i=0
+num=2
+while i<resul:
+    div=2
+    nprimo=0
+    while num>div:
+        if num%div==0:
+            nprimo+=1
+            break
+        else:
+            div+=1
+    if nprimo==0:
+        if resul-i==1:
+            print(num)
+        else:
+            print(num,end=", ")
+        num+=1
+        i+=1
+    else:
+        num+=1
